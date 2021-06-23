f19bf08ebdab21840d44e2f74bf18214120ee0f7
Rosierabelo <rabelo.rosi@gmail.com>
2021-06-23 11:23:57 -0300

    11:23

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Cadastro from './Login/Cadastro';
 import Navbar from './Navbar/Navbar';
-import Home from './Home/Home'
 import Login from './Login/Login';
 import Produtos from './Produtos/Produtos';
-<<<<<<< HEAD
 import Produto from './Produtos/ProdutoDetalhe';
-=======
->>>>>>> 7b1e2afa22b143f01a69fd4dc6dd7232a22b4263
 function App() {
   return (
     <BrowserRouter>
@@ -21,9 +17,6 @@ function App() {
         </Route>
         <Route exact path="/login">
           <Login/>
-        </Route>
-        <Route exact path="/produtos">
-          <Produtos/>
         </Route>
         <Route path="/produtos/:id">
           <Produto />
