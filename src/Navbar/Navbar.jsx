adbd68f0049e3d498571492d77eaf90e632853be
Cristiny C. Boêta <cristinybt20@gmail.com>
2021-06-23 10:26:45 -0300

    10:26 Cristiny

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,19 +1,23 @@
 import './Navbar.css'
 import { Link } from 'react-router-dom';
+import carrinho from './img1.png'
 
 
 const Navbar = () =>{
     return(
         <div>
-            <link href="/website/css/uicons-bold"
-            rel="stylesheet"></link>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><a href="#contact">Carrinho</a></li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/cadastro">Cadastre-se</Link></li>
-                <i class="fi-rr-shopping-cart"></i>
-            </ul>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><a href="#contact">Carrinho</a></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/cadastro">Cadastre-se</Link></li>
+                </ul>
+                <ul>
+                    <li><img className="carrinho" src={carrinho}></img></li>
+                </ul>
+            </nav>
+
         </div>
     )
 }
