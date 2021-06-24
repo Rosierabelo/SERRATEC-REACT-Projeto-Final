ad7710c41e54f5f2ef7afd4e9ffbbf28b9183ccc
Diego-Chirico <diegochiricofullstack@gmail.com>
2021-06-24 11:15:36 -0300

    pasta categoria

---
+++ src/Produtos/Categoria.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import {busca} from '../Http'
+import { Link } from 'react-router-dom'
+
+
+const Categoria = () => {
+    const [categorias, setCategorias] = useState([])
+    useEffect(() => {
+        busca(`/categoria`, setCategorias)
+    }, [])
+
+    return (
+        <ul className="lista-categorias container flex">
+            {
+                categorias.map((categoria) => (
+                    <Link to={`/categoria/${categoria.id}`}>
+                        <li className={`lista-categorias__categoria lista-categorias__categoria--${categoria.id}`}>
+                            {categoria.nome}
+                        </li>
+                    </Link>
+                ))
+            }
+        </ul>
+    )
+}
+
+export default Categoria
